efdbb063d7bb048e95fb815b6586759c4d1d9554
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-12-29 12:02:42 -0600

    partial

--- functions.js
+++ functions.js
@@ -1,13 +1,28 @@
-const identity = function () {
-
+const identity = function (value) {
+return value
 };
 
-const first = function () {
-  
+const first = function (array, n) {
+  if(n === undefined){
+    return array[0]
+  }  if (n === 0) {
+    return [];
+  } if (n > array.length) {
+    return array;
+} return array.slice(0, n);
 };
   
-const last = function () {
-  
+const last = function (array, n) {
+  if(n === undefined){
+    return array[array.length -1]
+  }
+  if(n === 0){
+    return []
+  }
+  if(n > array.length){
+    return array
+  }
+  return array.slice(array.length - n)
 };
 
 const each = function () {
